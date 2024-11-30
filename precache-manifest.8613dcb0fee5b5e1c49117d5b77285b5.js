72f5f384b1bef6cd33eee2896f30b11ca604033a
Oluwaseun Sodeeq <ademolaoluwaseun90@gmail.com>
2024-11-30 15:17:33 +0100

    Updates

---
+++ precache-manifest.8613dcb0fee5b5e1c49117d5b77285b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6581739a9126043ad715a987c6629d40",
+    "url": "/little-lemon/index.html"
+  },
+  {
+    "revision": "eaa119808e27e889ab8f",
+    "url": "/little-lemon/static/js/2.0956e000.chunk.js"
+  },
+  {
+    "revision": "7d4e8e12f94da3fb88b3",
+    "url": "/little-lemon/static/js/main.6262dd2c.chunk.js"
+  },
+  {
+    "revision": "eab72e9c680083adb518",
+    "url": "/little-lemon/static/js/runtime~main.4d886ec1.js"
+  }
+]);
